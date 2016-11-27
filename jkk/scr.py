5e32f359c83d5f2d5e687ccf9e0fe702160b35db
shrkw <shrkwh@gmail.com>
2016-11-28 01:10:10 +0900

    Refactor

--- jkk/scr.py
+++ jkk/scr.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 logger.addHandler(handler)
 
 def run(force_send=False):
-    co = get_collection('condition')
+    co = get_collection('conditions')
     for row in co.find():
         call(row['recipient'], row['q_word'])
 
@@ -22,9 +22,13 @@ def call(recipient, q_word):
     s = Searcher()
     soup = s.search(q_kana=q_word)
     err = soup.find('li', class_='error')
-    if err and u'空室はございません' in err.text:
-        send_search_result(recipient, u'JKK検索結果: 見つかりませんでした: %s' % q_word, { "title": u'JKK検索結果: 見つかりませんでした: %s' % q_word, "lead": u'10分おきに検索します', "query": q_word})
+    if err and u'空室' in err.text:
+        subject = u'JKK検索結果: 見つかりませんでした: %s' % q_word
+        lead = u'10分おきに検索します'
     elif err:
-        send_search_result(recipient, u'JKK検索結果: エラーがおきました: %s' % q_word, { "title": u'JKK検索結果: エラーがおきました: %s' % q_word, "lead": err.text, "query": q_word})
+        subject = u'JKK検索結果: エラーがおきました: %s' % q_word
+        lead = err.text
     else:
-        send_search_result(recipient, u'JKK検索結果: 見つかりました: %s' % q_word, { "title": u'JKK検索結果: 見つかりました: %s' % q_word, "lead": u'すぐにチェックして申し込みましょう', "query": q_word})
+        subject = u'JKK検索結果: 見つかりました: %s' % q_word
+        lead = u'すぐにチェックして申し込みましょう'
+    send_search_result(recipient, subject, { "title": subject, "lead": lead, "query": q_word})
