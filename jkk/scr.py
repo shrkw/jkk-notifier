84333366d2204db25a50e10364b455ac02bb2999
shrkw <shrkwh@gmail.com>
2016-12-12 23:21:43 +0900

    Extract method

--- jkk/scr.py
+++ jkk/scr.py
@@ -23,6 +23,17 @@ def call(force_send, id, recipient, q_word):
     s = Searcher()
     soup = s.search(q_kana=q_word)
     err = soup.find('li', class_='error')
+    subject, lead, found = collect(err, q_word)
+
+    co = get_collection('search_histories')
+    rows = list(co.find({"condition_id": id}).sort('_id', pymongo.DESCENDING).limit(1))
+    co.insert_one({"found": found, "subject": subject, "q_word": q_word})
+    # treat the status is changed when first search
+    status_changed = len(rows) == 0 or rows[0]['found'] != found
+    if force_send or status_changed:
+        send_search_result(recipient, subject, { "title": subject, "condition_id": id, "lead": lead, "query": q_word})
+
+def collect(err, q_word):
     if err and u'空室' in err.text:
         subject = u'JKK検索結果: 見つかりませんでした: %s' % q_word
         lead = u'10分おきに検索します'
@@ -35,10 +46,4 @@ def call(force_send, id, recipient, q_word):
         subject = u'JKK検索結果: 見つかりました: %s' % q_word
         lead = u'すぐにチェックして申し込みましょう'
         found = True
-    co = get_collection('search_histories')
-    rows = list(co.find({"condition_id": id}).sort('_id', pymongo.DESCENDING).limit(1))
-    co.insert_one({"found": found, "subject": subject, "q_word": q_word})
-    # treat the status is changed when first search
-    status_changed = len(rows) == 0 or rows[0]['found'] != found
-    if force_send or status_changed:
-        send_search_result(recipient, subject, { "title": subject, "condition_id": id, "lead": lead, "query": q_word})
+    return (subject, lead, found)
