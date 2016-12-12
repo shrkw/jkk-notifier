3a05b6d0b1785e266e97fdc3081a82f201c87f7b
shrkw <shrkwh@gmail.com>
2016-12-12 23:23:28 +0900

    condition_id should be passed to search_histories

--- jkk/scr.py
+++ jkk/scr.py
@@ -19,19 +19,19 @@ def run(force_send=False):
     for row in co.find():
         call(force_send, str(row['_id']), row['recipient'], row['q_word'])
 
-def call(force_send, id, recipient, q_word):
+def call(force_send, id_, recipient, q_word):
     s = Searcher()
     soup = s.search(q_kana=q_word)
     err = soup.find('li', class_='error')
     subject, lead, found = collect(err, q_word)
 
     co = get_collection('search_histories')
-    rows = list(co.find({"condition_id": id}).sort('_id', pymongo.DESCENDING).limit(1))
-    co.insert_one({"found": found, "subject": subject, "q_word": q_word})
+    rows = list(co.find({"condition_id": id_}).sort('_id', pymongo.DESCENDING).limit(1))
+    co.insert_one({"condition_id": id_, "found": found, "subject": subject, "q_word": q_word})
     # treat the status is changed when first search
     status_changed = len(rows) == 0 or rows[0]['found'] != found
     if force_send or status_changed:
-        send_search_result(recipient, subject, { "title": subject, "condition_id": id, "lead": lead, "query": q_word})
+        send_search_result(recipient, subject, { "title": subject, "lead": lead, "query": q_word})
 
 def collect(err, q_word):
     if err and u'空室' in err.text:
